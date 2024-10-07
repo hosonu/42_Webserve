86ed83fddcd0ff99ad2fd3e22b4b4034d4e7e5d2
hosonu <hosonoyuki07@gmail.com>
2024-10-07 15:31:42 +0900

    setting config class and serverconfig

---
+++ src/config/Config.hpp
@@ -0,0 +1,16 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <cstring>
+
+class Config {
+	public:
+		Config();
+		~Config();
+
+		// bool	parse(const std::string &filePath);
+	private:
+
+};
+
+#endif
---
+++ src/config/ServerConfig.hpp
@@ -0,0 +1,25 @@
+#ifndef SERVERCONFIG_HPP
+#define SERVERCONFIG_HPP
+
+#include <cstring>
+#include <map>
+
+class ServerConfig {
+	public:
+	private:
+		//server definition data & groval settigns data
+		int	listenPort;
+		std::string host;
+		std::string serverName;
+		std::map<int, std::string> errorPages;
+		std::string maxBodySize;//it's okay to get num(size_t or int)
+
+
+		/*we should make route class or route data*/
+		//route settings data
+		std::string rootPath;
+		//etc
+
+}
+
+#endif
