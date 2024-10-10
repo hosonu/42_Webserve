4f489fe5a95b7efa432fa7b30be1ff08541ec26f
hosonu <hosonoyuki07@gmail.com>
2024-10-10 22:33:33 +0900

    complete checks&get filePath

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,38 @@
 #include "core/Server.hpp"
 #include "config/Config.hpp"
 
-bool checkArgv(int argc, char *argv[]) {
+bool getValidatePath(int argc, char *argv[], std::string &path) {
+	
 	if (argc == 1) {
-		return 1;
-	 } else if(argc != 2) {
+		path = "config/test.conf";
+	}  else if (argc == 2) {
+		path = std::string(argv[1]);
+	} else {
 		std::cerr << "ERROR: too many arguments" << std::endl;
 		return false;
-	 }
-	 return true;
-}
-
-static std::string	getPath(int argc, char *argv[]) {
-	std::string path;
-
-	if (argc == 1) {
-		path = "../config/default.conf";
-	} else if (argc == 2) {
-		path = std::string(argv[1]);
 	}
-	return path;
+
+	if (path.size() >= 5 && path.substr(path.size() - 5) == ".conf") {
+		return true;
+	} else {
+		std::cerr << "ERROR: The extension must be \".conf\".\n";
+        return false;
+	}
 }
 
 int main(int argc, char *argv[]){
+	
+	std::string path;
 
-	if (checkArgv(argc, argv) == false) {
+	if (getValidatePath(argc, argv, path) == false) {
 		return false;
 	}
-	std::string path(getPath(argc, argv));
-
+ 
 	Config conf;
-	conf.parse(path);
+	if (conf.parse(path) == false) {
+		return false;
+	} 
+
 	try {
 		Server s(conf);
 		s.setServer();
